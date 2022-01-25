9e672fba9ee7e8bca594df538b9751d619facfc8
pathi-manoj <91237206+pathi-manoj@users.noreply.github.com>
2022-01-25 23:24:53 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <assert.h>
-
+#define CELSIUS_THRESHOLD_ALERT = 200;
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
     std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
+    if( celsius < CELSIUS_THRESHOLD_ALERT)
+    {
+       // Return 200 for ok
+       return 200;
+    }
+    else
+    {
+       // Return 500 for not-ok
+       return 500;
+    }
 }
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
+        // non-ok response is alert!
         alertFailureCount += 0;
     }
 }
@@ -27,6 +30,7 @@ int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
     std::cout << alertFailureCount << " alerts failed.\n";
+    assert(alertFailureCount == 0);
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
